f32c82150344bb857d563d01d9c77d0f5328a9b7
Pleasure Ndhlovu <Pleasure.Ndhlovu@Derivco.co.za>
2025-08-06 15:35:02 +0200

    test: GetSolutionDirectory helper added

--- test/GuardAppSettings.Tests/Helpers.cs
+++ test/GuardAppSettings.Tests/Helpers.cs
@@ -2,4 +2,19 @@
 
 public static class Helpers
 {
+    public static string GetSolutionDirectory()
+    {
+        var startPath = Directory.GetCurrentDirectory();
+        var directory = new DirectoryInfo(startPath);
+
+        while (directory != null)
+        {
+            if (directory.GetFiles("*.sln").Length > 0)
+                return directory.FullName;
+
+            directory = directory.Parent;
+        }
+
+        throw new FileNotFoundException("Solution file not found in the directory hierarchy.", startPath);
+    }
 }
